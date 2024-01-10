4d2a317d9c5de63cf3bef28da1c25dc785d5c79e
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-10 21:24:37 +0300

    Add project files.

---
+++ StringSinifiMetotlari/Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+
+// String sınıfı metotları ile string üzerinde bazı işlemleri yapabileceksiniz
+// Bu işlemler ;
+
+string stringMetin = " Bu bir string ifadedir ";
+
+// contans bir metinin içerisinde bir harf yada bir kelime arama için kullanılır.
+//bool isContains =stringMetin.Contains("iki");
+//bool isok = stringMetin.Contains("bir");
+//Console.WriteLine(isContains);
+//Console.WriteLine(isok);
+
+
+//StartWith
+// bu metot bir string ifadesinin baş harfinin karakter kontrolü yapmaktadır
+//bool isok = stringMetin.StartsWith("B");
+//Console.WriteLine(isok); 
+
+//EndWith 
+// bu metot bir string ifadenin son harfini kontrol eder
+
+//bool isok = stringMetin.EndsWith("dedir");
+//Console.WriteLine(isok);
+
+// Insert 
+// Insert bir metin içerisine belirli bir index numarasından sonra istediğimiz değeri eklemek için kullanılabilir
+
+//string newString = stringMetin.Insert(5, "bu bir insert olayıdır");
+//Console.WriteLine(newString);
+
+
+//Remove //Remove bir metinsel ifadenin içerisindeki belirli bölümleri silmek için kullanılır
+
+//string newString = stringMetin.Remove(5, 2);
+//Console.WriteLine(newString); 
+
+
+//SubString
+//Substring kelimenin istenilen parçasını almak için kullanılır
+//string newString = stringMetin.Substring(0, 5);
+//Console.WriteLine(newString);
+
+//ToUpper
+// Küçük harfleri büyük harfe çevirir
+
+
+//string newString = stringMetin.ToUpper();
+//Console.WriteLine(newString);
+
+
+// ToLower
+// büyük harfleri küçük harflere çevirmek için kullanılır.
+
+//Split
+// Split metodu bir metinsel ifade de her kelime arasına bir karakter yerleştirmek için kullanılır
+
+
+
+//// Sonra bakalım
+//string[] newString = stringMetin.Split('-', StringSplitOptions.None, StringSplitOptions.TrimEntries);
+//for (int i = 0; i < newString.Length; i++)
+//{
+
+//    Console.WriteLine(newString[i]);
+//}
+
+
+//Trim
+//Trim metinsel değişkenin üzerindeki son ve baştaki boşluk karakterini temizler
+//string newString = stringMetin.Trim();
+//Console.WriteLine(newString);
+
+
+//TrimStart ve TrimEnd bu metotlarda karakterin başındaki yada sonundaki boşluk ifadelerini kaldırır
